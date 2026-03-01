6280391cd346fe10af28b0c1b1c4c6ff7590d219
Thomas Reddington <active_evil@protonmail.com>
2026-03-01 13:46:45 +0000

    Cp

--- components/CardRandomisers.tsx
+++ components/CardRandomisers.tsx
@@ -24,7 +24,7 @@ export const StrategyCardRandomiser: React.FC<{
 
   return (
     <>
-      <div className="-mx-4 md:-mx-8 flex flex-col items-start gap-4 pt-4 px-4 md:px-8 bg-2ed-mid-blue border-t-4 border-black shadow-lg">
+      <div className="-mx-4 md:-mx-8 flex flex-col items-start gap-4 pt-4 px-4 md:px-8 bg-2ed-mid-blue/99 border-t-4 border-black shadow-lg">
         <p className="text-xl">
           Select which card decks you wish to include and randomly draw one
           card.
@@ -76,7 +76,7 @@ export const StrategyCardRandomiser: React.FC<{
         </div>
       </div>
       <div className="sticky top-0 z-10 -mx-4 md:-mx-8 -mt-8 bg-background border-b-4 border-black shadow-lg">
-        <div className="flex flex-col items-start gap-4 p-4 md:px-8 w-full h-full bg-2ed-mid-blue ">
+        <div className="flex flex-col items-start gap-4 p-4 md:px-8 w-full h-full bg-2ed-mid-blue/99">
           <Link
             className="px-4 py-1 rounded-none bg-2ed-light-blue border-4 border-black outline-0 text-black font-subtitle shadow-lg"
             href={baseHref}
