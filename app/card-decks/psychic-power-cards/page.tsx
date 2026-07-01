6bdc614e9b37673805da24de0ab59446fe922de2
Thomas Reddington <active_evil@protonmail.com>
2026-07-01 16:05:00 +0100

    CP

--- app/card-decks/psychic-power-cards/page.tsx
+++ app/card-decks/psychic-power-cards/page.tsx
@@ -1,10 +1,10 @@
 import { Breadcrumbs } from "@/components/Breadcrumbs";
-import { StrategyCardRandomiser } from "@/components/CardRandomisers";
 import { Highlighter } from "@/components/Highlighter";
 import { ImageWithCredit } from "@/components/Image";
 import { Database } from "@/database.types";
 import { createClient } from "@supabase/supabase-js";
 import { clsx } from "clsx";
+import Link from "next/link";
 import { Metadata } from "next/types";
 
 export const deckColors: Record<string, string> = {
@@ -42,17 +42,17 @@ export default async function Page() {
     .order("id");
 
   if (heros?.length && psychic_power_cards) {
-    const decks = new Map<string, typeof psychic_power_cards>();
+    const cards = new Map<string, typeof psychic_power_cards>();
 
     for (const item of psychic_power_cards) {
-      const bucket = decks.get(item.deck) ?? [];
+      const bucket = cards.get(item.deck) ?? [];
       bucket.push(item);
-      decks.set(item.deck, bucket);
+      cards.set(item.deck, bucket);
     }
 
-    const cards = Array.from(decks.entries()).map(([deck, items]) => ({
-      deck,
-      items,
+    const decks = Array.from(cards.entries()).map(([name, cards]) => ({
+      name,
+      cards,
     }));
 
     return (
@@ -91,19 +91,45 @@ export default async function Page() {
               </div>
             ))}
           </div>
-          {cards.map((section) => {
-            const deckId = section.deck.split(" ").join("_");
+          <div className="relative flex flex-col items-center justify-center gap-4 w-full">
+            <hr className="md:absolute -z-10 max-w-5xl w-[calc(100vw-var(--spacing)*4)] md:w-[calc(100vw-var(--spacing)*8)] h-1 bg-black border border-black shadow-lg" />
+            <h2 className="md:px-2 bg-background font-title text-3xl text-center uppercase">
+              Psychic Disciplines
+            </h2>
+          </div>
+          <nav className="ordered-list">
+            <ol className="flex flex-col gap-2 text-2xl">
+              {decks.map(({ name }) => {
+                const deckId = name.split(" ").join("_");
+                const href = `/card-decks/psychic-power-cards#${deckId}`;
+
+                return (
+                  <li key={deckId}>
+                    <Link
+                      className="font-subtitle hover:underline underline-offset-4"
+                      href={href}
+                    >
+                      {name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ol>
+          </nav>
+
+          {decks.map((deck) => {
+            const deckId = deck.name.split(" ").join("_");
 
             return (
               <section id={deckId} key={deckId} className="flex flex-col gap-4">
                 <div className="relative flex flex-col items-center justify-center gap-4 w-full">
                   <hr className="md:absolute -z-10 max-w-5xl w-[calc(100vw-var(--spacing)*4)] md:w-[calc(100vw-var(--spacing)*8)] h-1 bg-black border border-black shadow-lg" />
                   <h2 className="md:px-2 bg-background font-title text-3xl text-center uppercase">
-                    {section.deck}
+                    {deck.name}
                   </h2>
                 </div>
                 <section className="grid md:grid-cols-2 gap-4">
-                  {section.items.map((card) => {
+                  {deck.cards.map((card) => {
                     const cardId = card.name.split(" ").join("_");
 
                     return (
@@ -112,7 +138,7 @@ export default async function Page() {
                         id={cardId}
                         className={clsx(
                           "flex flex-col justify-start items-center gap-2 p-4 border-4 border-black target:border-2ed-light-yellow shadow-xl",
-                          deckColors[section.deck],
+                          deckColors[deck.name],
                         )}
                       >
                         <div className="flex flex-col justify-start items-center gap-4 p-4 h-full bg-2ed-white text-2ed-black">
