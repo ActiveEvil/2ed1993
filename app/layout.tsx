def9eb9bf09678ee61bee7ec3a4ad0796dbb5b7c
Thomas Reddington <active_evil@protonmail.com>
2026-08-08 15:47:05 +0100

    CP

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,9 +73,10 @@ export default async function RootLayout({
         </div>
         <footer className="p-4 mb-4 font-title text-xs text-center">
           ©{new Date().getFullYear()} 2ed1993 (the Warhammer 40,000 2nd Edition
-          digital record project). All rights reserved. Credited images are the
-          property of their respective copyright holders and are reproduced for
-          reference only..
+          digital record project). All rights reserved.
+          <br />
+          Credited images are the property of their respective copyright holders
+          and are reproduced for reference only..
         </footer>
         <Analytics />
       </body>
