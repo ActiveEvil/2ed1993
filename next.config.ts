e8432019d4894ad6e751c3a33958129c43a19956
Thomas Reddington <active_evil@protonmail.com>
2026-08-19 07:11:24 +0100

    CP

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline'${process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : ""};
+    script-src 'self' 'unsafe-inline'${process.env.NODE_ENV === "development" ? " 'unsafe-eval' https://va.vercel-scripts.com" : ""};
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: https://twcioksmkijgnhgxibrn.supabase.co;
     font-src 'self';
@@ -61,10 +61,10 @@ const nextConfig: NextConfig = {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
           },
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value: cspHeader.replace(/\n/g, ""),
+          // },
         ],
       },
     ];
