dc61e09d7ff18b0cbdfa5b7f801d5bf6a6fbf3ba
Æ <ActiveEvil@users.noreply.github.com>
2026-08-28 22:40:04 +0100

    Update page.tsx

--- app/factions/[faction]/[list]/page.tsx
+++ app/factions/[faction]/[list]/page.tsx
@@ -593,7 +593,7 @@ export default async function Page(props: {
                                     className="group flex min-w-0 flex-col gap-1 py-4 px-4 md:px-8 target:bg-2ed-light-yellow target:text-black"
                                   >
                                     <div className="flex items-baseline gap-x-3 text-lg">
-                                        <h4 className="font-subtitle text-xl md:text-2xl">
+                                        <h4 className="font-subtitle text-lg md:text-2xl">
                                           <HighlighterLink
                                             className="hover:underline underline-offset-4"
                                             href={`${listHref}#${entry.anchor}`}
